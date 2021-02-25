a5187867dc2d6d44495b3da50aaad4d05d7721a8
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-02-25 23:16:42 +0200

    first part of Lab 4 done - fulfills all the requirements

--- ICD0007_LAB4/index.php
+++ ICD0007_LAB4/index.php
@@ -1,1 +1,79 @@
-<h1>Hello php!</h1>
+<?php
+error_reporting(-1);
+const KM_TO_MILE = 1.60934;
+
+function make_random($each)
+{
+  return ($each*rand(1, 100));
+}
+
+function from_km_to_mile($each)
+{
+  return ($each*KM_TO_MILE);
+}
+
+function getKey($testKey, &$arr)
+{
+  $keyCount = 0;
+  foreach($arr as $key=>$value)
+  {
+    $hayInt = intval($key);
+    if($testKey === $hayInt)
+    {
+      $keyCount ++;
+    }
+  }
+  $appendKey = ($keyCount > 0) ? chr(ord('A') + $keyCount - 1) : "";
+  return $testKey . $appendKey;
+}
+
+
+$numOfDistances = rand(5, 20);
+
+$arrDistances = array();
+
+for($i = 0; $i < $numOfDistances; $i++)
+{
+  array_push($arrDistances, make_random(1));
+}
+
+// I wanted to use functional programming but I'm not allowed to, makes me sad
+// $distance_array = array_map('make_random', array_fill(0, $len_array, 1));
+
+print_r($arrDistances);
+
+// I added 30 several times just so you could test in case you don't get two 
+// identical keys
+array_push($arrDistances, 30);
+array_push($arrDistances, 30);
+array_push($arrDistances, 30);
+array_push($arrDistances, 30);
+array_push($arrDistances, 30);
+
+sort($arrDistances, SORT_REGULAR);
+print_r($arrDistances);
+
+$arrMiles = array();
+
+for($i = 0; $i < count($arrDistances); $i++)
+{
+  $mileKey = getKey($arrDistances[$i], $arrMiles);
+  $arrMiles[$mileKey] = from_km_to_mile($arrDistances[$i]);
+}
+print("______________\n");
+print("| Km | Miles |\n");
+
+foreach($arrMiles as $km => $mile)
+{
+  $formattedMile = number_format($mile, 3);
+  $spaceOne = ($km < 100) ? ($km < 10) ? "  " : " " : "";
+  $spaceTwo = (intval($km) === $km) ? " ": "";
+  $spaceThree = ($formattedMile < 100) ? ($formattedMile < 10) ? "  " : " " : "";
+  printf("|%s%s%s|%s%s|\n", $spaceOne, $km, $spaceTwo, $spaceThree, $formattedMile);
+  
+}
+print("|____________|\n");
+
+
+echo "\n"
+?>

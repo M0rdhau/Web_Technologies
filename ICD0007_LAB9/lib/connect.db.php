26ff3a8b9d06d3adc882880f395747f5bee04115
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-04-20 22:52:13 +0300

    corrected db params for enos

--- ICD0007_LAB9/lib/connect.db.php
+++ ICD0007_LAB9/lib/connect.db.php
@@ -1,9 +1,9 @@
 <?php
-// const SERVER_ADDRESS = "anysql.itcollege.ee";
-// const USER_NAME = "team9";
-// const USER_PASSORD = "177df64d7391";
-// const DATABASE_NAME = "WT_9";
-const SERVER_ADDRESS = "localhost";
-const USER_NAME = "php_kvartz";
-const USER_PASSORD = "drowssap";
-const DATABASE_NAME = "webtech_labs";
+const SERVER_ADDRESS = "anysql.itcollege.ee";
+const USER_NAME = "team9";
+const USER_PASSORD = "177df64d7391";
+const DATABASE_NAME = "WT_9";
+// const SERVER_ADDRESS = "localhost";
+// const USER_NAME = "php_kvartz";
+// const USER_PASSORD = "drowssap";
+// const DATABASE_NAME = "webtech_labs";
